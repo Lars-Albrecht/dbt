dbcd0ca0dfc3b51060861405f00ffaeb24dd795c
Sebastian <sebastian@leslunes.de>
2021-07-21 09:52:58 +0200

    redirect to dbt models

--- models/staging/marketing/geckoboard_metrics_combined.sql
+++ models/staging/marketing/geckoboard_metrics_combined.sql
@@ -1,9 +1,9 @@
 SELECT date, sessions, users, sessions_with_product_views, sessions_with_add_to_cart, sessions_with_checkout, sessions_with_transactions, 'DE' AS source  
-FROM `leslunes-prep.marketing.geckoboard_metrics_de`
+FROM `leslunes-prep.dbt_marketing.geckoboard_metrics_de`
 UNION ALL
 SELECT date, sessions, users, sessions_with_product_views, sessions_with_add_to_cart, sessions_with_checkout, sessions_with_transactions, 'FR' AS source  
-FROM `leslunes-prep.marketing.geckoboard_metrics_fr`
+FROM `leslunes-prep.dbt_marketing.geckoboard_metrics_fr`
 UNION ALL
 SELECT date, sessions, users, sessions_with_product_views, sessions_with_add_to_cart, sessions_with_checkout, sessions_with_transactions, 'IT' AS source  
-FROM `leslunes-prep.marketing.geckoboard_metrics_it`
+FROM `leslunes-prep.dbt_marketing.geckoboard_metrics_it`
 
