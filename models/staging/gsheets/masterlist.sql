d72e03966616d569db3bb25f84c38499febf843a
Sebastian <sebastian@leslunes.de>
2021-07-21 13:54:22 +0200

    fixed missing cols in product masterlist

--- models/staging/gsheets/masterlist.sql
+++ models/staging/gsheets/masterlist.sql
@@ -1,4 +1,20 @@
 {{ config(materialized='table') }}
 
-SELECT * FROM `leslunes-raw.products.masterlist` WHERE product_name IS NOT NULL
-
+SELECT 
+  product_name,	
+  sku,	
+  colour,	
+  size,	
+  fabric,
+  gtin_ean,	
+  hs_code	,
+  product,
+  adapted_name,
+  Active_vs__Not_Active AS active_vs_not_active,	
+  Last_FOB____ AS last_fob_euro,
+  Last_FOB__ AS last_fob_dollars,	
+  last_fob_price_date		,
+  name_with_size	,
+  category	
+FROM `leslunes-raw.products.masterlist` 
+WHERE product_name IS NOT NULL
