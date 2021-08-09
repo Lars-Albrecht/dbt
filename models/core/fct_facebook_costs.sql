a6e1064f2732133993f4db5f914f53f80e295a95
Sebastian <sebastian@leslunes.de>
2021-08-09 11:52:00 +0200

    add facebook costs

---
+++ models/core/fct_facebook_costs.sql
@@ -0,0 +1,23 @@
+WITH old_deal AS(
+SELECT date, payout_fb AS spend 
+FROM `leslunes-prep.dbt_costs.stg_facebook_costs_stoyo`
+WHERE date <= "2021-06-06"
+),
+
+new_deal AS (
+SELECT date, payout_code_redemptions AS spend
+FROM `leslunes-prep.dbt_costs.stg_facebook_costs_stoyo`
+WHERE date > "2021-06-06"
+), 
+
+own_try AS(
+SELECT date, total_spend AS spend
+FROM `leslunes-prep.dbt_costs.stg_facebook_costs`
+)
+
+SELECT * FROM old_deal
+UNION ALL
+SELECT * FROM new_deal
+UNION ALL
+SELECT * FROM own_try
+ORDER BY DATE DESC
