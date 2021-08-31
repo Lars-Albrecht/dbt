bcf135bff8b164e17e8ee645155420959010679f
Rodrigo Benitez <62714022+rodrigorbenitez@users.noreply.github.com>
2021-08-31 13:35:32 +0200

    modify items_quantity_by_colour_per_month motel and add items_quantit… (#25)
    * modify items_quantity_by_colour_per_month motel and add items_quantity_per_month model
    
    * modify sales_reports models add items_to_customers_infl_per_month model
    
    * add items_to_influencers_per_month model

--- models/staging/sales_reports/items_to_influencers_per_month.sql
+++ models/staging/sales_reports/items_to_influencers_per_month.sql
@@ -1,8 +1,7 @@
 SELECT
   type,
   SUM(line_items__quantity) AS line_items__quantity,
-  colour,
-  {% set col_dict = dbt_utils.get_query_results_as_dict("
+    {% set col_dict = dbt_utils.get_query_results_as_dict("
 WITH days AS (
 SELECT * FROM UNNEST(GENERATE_DATE_ARRAY((SELECT min(date(created_at)) FROM `leslunes-prep.dbt_orders.orders_with_items_combined`), current_date()+31, INTERVAL 1 month)) AS day
 )
@@ -16,8 +15,7 @@ END
 {%-if not loop.last -%}
 ,
 {%- endif -%}
-{%- endfor %}
-
+{%- endfor %} 
 FROM (
   SELECT
     (CASE
@@ -43,7 +41,7 @@ FROM (
         WHEN line_items__sku LIKE '%VALERY%' THEN 'The Valery Scarf'
         WHEN line_items__sku LIKE '%LUNA%' THEN 'Luna Leggings'
         WHEN line_items__sku LIKE '%ABBY%' THEN 'Abby'
-        WHEN line_items__sku LIKE  '%CARA2% 'THEN 'Cara_2'
+        WHEN line_items__sku LIKE '%CARA2%' THEN 'Cara_2'
         WHEN line_items__sku LIKE '%CARA%' THEN 'Cara'
         WHEN line_items__sku LIKE '%LOU%' THEN 'Lou'
         WHEN line_items__sku LIKE '%BELLE%' THEN 'Belle'
@@ -62,45 +60,17 @@ FROM (
     line_items__sku,
     SUM(SAFE_CAST(line_items__quantity AS INT64)) AS line_items__quantity,
     SUBSTR(SAFE_CAST(created_at AS STRING),1,7) AS order_date,
-    colour
-  FROM (
-    SELECT
-      line_items__name,
-      line_items__quantity,
-      created_at,
-      CASE
-        WHEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Z \d\W]+'),-3)='-2-' 
-          THEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Z \d\W]+'),1,LENGTH(REGEXP_EXTRACT(line_items__sku, r'^[A-Z \d\W]+'))-3)
-        WHEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Z \d\W]+'),-1)='-' 
-          THEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Z \d\W]+'),1,LENGTH(REGEXP_EXTRACT(line_items__sku, r'^[A-Z \d\W]+'))-1)
-      ELSE
-      REGEXP_EXTRACT(line_items__sku, r'^[A-Z \d\W]+')
-    END
-      AS line_items__sku
-    FROM
-     `leslunes-prep.dbt_orders.orders_with_items_combined` #`les-lunes-data-269915.import_combined.unique_orders_with_items`
-    WHERE
-      (source_name='web')
-      AND line_items__sku!='' AND UPPER(line_items__sku) NOT LIKE '%SET%') AS orders
-  LEFT JOIN (
-    SELECT
-      sku,
-      product_name,
-      size,
-      colour
-    FROM
-      `leslunes-raw.products.masterlist`  #import.products_masterlist
-      ) AS list
-  ON
-    orders.line_items__sku=list.sku
+  FROM
+    `leslunes-prep.dbt_orders.orders_with_items_combined` #`les-lunes-data-269915.import_combined.unique_orders_with_items`
+  WHERE
+    (UPPER(note) LIKE '%INFLUENCE%'
+      OR UPPER(tags) LIKE '%INFLUENCE%')
+    AND line_items__sku!='' AND UPPER(line_items__sku) NOT LIKE '%SET%'
   GROUP BY
     order_date,
     line_items__sku,
-    type,
-    colour ) AS sales
+    type ) AS sales
 GROUP BY
-  type,
-  colour
+  type
 ORDER BY
-  type,
-  colour
+  type
