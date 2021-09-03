745e6450b734b7bb95dcdd8cef1d9b99d7f46b8b
Sebastian <sebastian@leslunes.de>
2021-09-03 15:21:45 +0200

    add sessions for product pages and influencer

--- models/staging/gsheets/streamline_po_data.sql
+++ models/staging/gsheets/streamline_po_data.sql
@@ -1,4 +1,7 @@
 {{ config(materialized='table') }}
 
 
-SELECT *, CURRENT_TIMESTAMP() as bq_updated_at FROM  `leslunes-raw.gsheets.streamline_po_data`
+SELECT 
+    *, CURRENT_TIMESTAMP() as bq_updated_at 
+FROM  
+    `leslunes-raw.gsheets.streamline_po_data`
