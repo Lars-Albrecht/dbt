1ef319dc6b0e6d0ce8a067cab492cead66d3f24b
Sebastian Schult <sebastian@leslunes.de>
2021-09-20 14:03:03 +0000

    move Italian GA test to archive

--- archive/tests/google_analytics_every_day_it.sql
+++ archive/tests/google_analytics_every_day_it.sql
@@ -1,6 +1,6 @@
 WITH date_array AS(
     SELECT 
-    /* GA data before was invalid */
+    -- GA data before was invalid 
     GENERATE_DATE_ARRAY('2021-01-01', DATE_SUB(current_date(),INTERVAL 1 DAY), INTERVAL 1 DAY) AS days
 ), days AS(
     SELECT regexp_replace(SAFE_CAST(days AS STRING), "-", "") AS day
@@ -11,4 +11,3 @@ SELECT *
 FROM days
 WHERE day not in (SELECT date
 FROM `leslunes-raw.ga_kpi_metrics.report_it`)
-
