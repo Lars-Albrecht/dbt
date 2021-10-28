3914f181c17a65038c9b902df576196940b2c54e
Sebastian <sebastian@leslunes.de>
2021-10-28 10:10:38 +0200

    finance transactions models

---
+++ models/staging/transactions/stg_sale.sql
@@ -0,0 +1,19 @@
+SELECT 
+  id,
+  user_id,
+  order_id,
+  TIMESTAMP(created_at) AS created_at,
+  kind,
+  status,
+  error_code,
+  amount,
+  currency,
+  gateway,
+  test,
+  _sdc_shop_myshopify_domain,
+  _sdc_shop_name,
+  source_name,
+  message,
+  parent_id
+FROM {{ ref('stg_transaction') }}
+WHERE kind = "sale"
