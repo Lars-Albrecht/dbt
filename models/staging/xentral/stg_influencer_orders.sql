9930f513e13f580696cbb97bbd7df771f5d6259f
Sebastian <sebastian@leslunes.de>
2021-11-03 14:41:40 +0100

    add influencer orders from old xentral instance

--- models/staging/xentral/stg_influencer_orders.sql
+++ models/staging/xentral/stg_influencer_orders.sql
@@ -1,11 +1,23 @@
 SELECT 
     SAFE_CAST(Datum AS DATE) AS Datum
-    ,Internet
+    ,CAST(Internet AS STRING) AS Internet
     ,Status
+    ,Name
+    ,CAST(Belegnr AS STRING) AS Belegnr
+    ,Bezeichnung	
+    ,Nummer
+    ,Internetseite
+    ,Bearbeiter
+FROM {{ source('xentral', 'influencer_orders') }}
+UNION ALL
+SELECT 
+    SAFE_CAST(Datum AS DATE) AS Datum
+    ,Internet
+    ,CASE WHEN Status not in ('storniert', 'abgeschlossen') THEN "unknown" ELSE Status END AS Status
     ,Name
     ,Belegnr
     ,Bezeichnung	
     ,Nummer
     ,Internetseite
     ,Bearbeiter
-FROM {{ source('xentral', 'influencer_orders') }}
+FROM {{ source('xentral', 'influencer_orders_old') }}
