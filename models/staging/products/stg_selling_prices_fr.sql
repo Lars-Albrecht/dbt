22b1ccc53eccee084cd004322a2e4a45c7324629
Sebastian <sebastian@leslunes.de>
2021-11-12 15:44:16 +0100

    update price history table

--- models/staging/products/stg_selling_prices_fr.sql
+++ models/staging/products/stg_selling_prices_fr.sql
@@ -1,5 +1,7 @@
 WITH excerpt AS(
 SELECT DISTINCT
+  id AS product_id,
+  v.value.id AS variant_id,
   DATETIME(v.value.created_at) AS created_at,
   v.value.sku,  
   v.value.price, 
@@ -7,20 +9,35 @@ SELECT DISTINCT
 FROM `leslunes-raw.shopify_fr.products` 
 LEFT JOIN UNNEST(variants) v
 WHERE v.value.sku != ""
+ORDER BY sku ASC, updated_at ASC
 )
 
 , final AS(
-
-SELECT DISTINCT
-  sku,
-  price AS new_price,
-  CASE WHEN lead(price) OVER(PARTITION BY sku ORDER BY updated_at DESC) IS NULL
-    THEN created_at ELSE updated_at END AS price_valid_from,
-  lead(price) OVER(PARTITION BY sku ORDER BY updated_at DESC) AS old_price
-FROM excerpt 
+  SELECT DISTINCT
+    product_id,
+    variant_id,
+    sku,
+    price AS new_price,
+    CASE WHEN lead(price) OVER(PARTITION BY sku, product_id,variant_id ORDER BY updated_at DESC) IS NULL
+      THEN created_at 
+      ELSE updated_at 
+    END AS price_valid_from,
+    lead(price) OVER(PARTITION BY sku, product_id,variant_id ORDER BY updated_at DESC) AS old_price
+  FROM excerpt
+  ORDER BY price_valid_from ASC
 )
 
-SELECT * 
+SELECT
+    product_id,
+    variant_id,
+    sku,
+    new_price, 
+    old_price, 
+    price_valid_from,
+    CASE WHEN LEAD(price_valid_from) OVER(PARTITION BY sku, product_id,variant_id ORDER BY price_valid_from ASC) IS NULL 
+    THEN CURRENT_DATETIME() 
+    ELSE LEAD(price_valid_from) OVER(PARTITION BY sku, product_id,variant_id ORDER BY price_valid_from ASC)
+    END AS price_valid_till
 FROM final 
 WHERE  new_price != old_price OR old_price IS NULL
 ORDER BY DATETIME(price_valid_from) ASC, sku
