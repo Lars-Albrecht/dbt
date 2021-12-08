962a0e4c1da0259efb030074632b153b6a23231b
Sebastian <sebastian@leslunes.de>
2021-12-08 10:26:02 +0100

    bugfix exchange rate error in masterlist

--- models/staging/gsheets/masterlist.sql
+++ models/staging/gsheets/masterlist.sql
@@ -11,8 +11,10 @@ SELECT
   product,
   adapted_name,
   active_vs_not_active,	
-  CAST(last_fob_euro AS FLOAT64) AS last_fob_euro,
-  CAST(last_fob_dollar AS FLOAT64) AS last_fob_dollar,	
+  # error in retrieving exchange rate
+  #SAFE_CAST(last_fob_euro AS FLOAT64) AS last_fob_euro,
+  NULL AS last_fob_euro,
+  SAFE_CAST(last_fob_dollar AS FLOAT64) AS last_fob_dollar,	
   last_fob_price_date,
   name_with_size,
   category	
