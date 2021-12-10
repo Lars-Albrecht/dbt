c92957906f64af27a92846ce7bc847849ebdb504
bamboo-ll-dev <79466398+bamboo-ll-dev@users.noreply.github.com>
2021-12-10 10:42:49 +0100

    facebook costs code update (#45)

--- models/core/fct_facebook_costs.sql
+++ models/core/fct_facebook_costs.sql
@@ -1,18 +1,30 @@
 WITH old_deal AS(
-SELECT date, payout_fb AS spend 
-FROM `leslunes-prep.dbt_costs.stg_facebook_costs_stoyo`
-WHERE date <= "2021-06-06"
+    # facebook budget spend by Stoyo agency
+SELECT 
+    date, 
+    payout_fb AS spend 
+FROM 
+    `leslunes-prep.dbt_costs.stg_facebook_costs_stoyo`
+WHERE 
+    date <= "2021-06-06"
 ),
 
 new_deal AS (
-SELECT date, payout_code_redemptions AS spend
-FROM `leslunes-prep.dbt_costs.stg_facebook_costs_stoyo`
-WHERE date > "2021-06-06"
+SELECT 
+    date, 
+    payout_code_redemptions AS spend
+FROM 
+    `leslunes-prep.dbt_costs.stg_facebook_costs_stoyo`
+WHERE 
+    date > "2021-06-06"
 ), 
 
 own_try AS(
-SELECT date, total_spend AS spend
-FROM `leslunes-prep.dbt_costs.stg_facebook_costs`
+SELECT 
+    date, 
+    total_spend AS spend
+FROM 
+    `leslunes-prep.dbt_costs.stg_facebook_costs`
 )
 
 SELECT * FROM old_deal
