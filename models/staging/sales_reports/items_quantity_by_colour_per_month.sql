deef1d2ee9cf0fb03af25f2023a5fe148cc4d18c
Jurate <jurate@leslunes.de>
2022-01-03 12:32:56 +0100

    changing source data for a query for sales report

--- models/staging/sales_reports/items_quantity_by_colour_per_month.sql
+++ models/staging/sales_reports/items_quantity_by_colour_per_month.sql
@@ -4,7 +4,7 @@ SELECT
   colour,
   {% set col_dict = dbt_utils.get_query_results_as_dict("
 WITH days AS (
-SELECT * FROM UNNEST(GENERATE_DATE_ARRAY((SELECT min(date(created_at)) FROM `leslunes-prep.dbt_orders.orders_with_items_combined`), current_date()+31, INTERVAL 1 month)) AS day
+SELECT * FROM UNNEST(GENERATE_DATE_ARRAY((SELECT min(date(created_at)) FROM `leslunes-prep.dbt_orderitems.stg_orderitems_combined`), current_date()+31, INTERVAL 1 month)) AS day
 )
 select DISTINCT(SUBSTR(SAFE_CAST(day AS STRING),1,7)) as day from days
 ") %}
@@ -30,25 +30,14 @@ FROM (
     SUM(SAFE_CAST(line_items__quantity AS INT64)) AS line_items__quantity,
     SUBSTR(SAFE_CAST(created_at AS STRING),1,7) AS order_date,
     colour
-  FROM (
-    SELECT
-      line_items__name,
-      line_items__quantity,
-      created_at,
-      CASE
-        WHEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Za-z \d\W]+'),-3)='-2-' 
-          THEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Za-z \d\W]+'),1,LENGTH(REGEXP_EXTRACT(line_items__sku, r'^[A-Za-z \d\W]+'))-3)
-        WHEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Za-z \d\W]+'),-1)='-' 
-          THEN SUBSTR(REGEXP_EXTRACT(line_items__sku, r'^[A-Za-z \d\W]+'),1,LENGTH(REGEXP_EXTRACT(line_items__sku, r'^[A-Za-z \d\W]+'))-1)
-      ELSE
-      REGEXP_EXTRACT(line_items__sku, r'^[A-Za-z \d\W]+')
-    END
-      AS line_items__sku
-    FROM
-     `leslunes-prep.dbt_orders.orders_with_items_combined` #`les-lunes-data-269915.import_combined.unique_orders_with_items`
-    WHERE
-      (source_name='web')
-      AND line_items__sku!='' AND UPPER(line_items__sku) NOT LIKE '%SET%') AS orders
+  FROM (SELECT 
+    qty AS line_items__quantity,
+    created_at,
+    sku AS line_items__sku
+FROM `leslunes-prep.dbt_orderitems.stg_orderitems_combined` 
+WHERE 
+    item_type!='set' 
+        AND source_name='web') AS orders
   LEFT JOIN (
     SELECT
       sku,
