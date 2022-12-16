5f71d8a6a717310f3e4fb4859cad95202da19f34
DQleslunes <97900750+DQleslunes@users.noreply.github.com>
2022-12-16 11:50:18 +0100

    extend schema by additional columns (#51)
    Co-authored-by: Stefan Worthmann <admin@leslunes.de>

--- models/staging/gsheets/masterlist.sql
+++ models/staging/gsheets/masterlist.sql
@@ -17,6 +17,18 @@ SELECT
   SAFE_CAST(last_fob_dollar AS FLOAT64) AS last_fob_dollar,	
   last_fob_price_date,
   name_with_size,
-  category	
+  category,
+  selling_status,
+  SAFE_CAST(last_fob AS FLOAT64) AS last_fob,
+  launch,
+  sub_category,
+  SAFE_CAST(weight AS float64) AS weight,
+  product_color,
+  SAFE_CAST(price_de AS float64) AS price_de,
+  SAFE_CAST(price_fr AS float64) AS price_fr,
+  SAFE_CAST(price_pl AS float64) AS price_pl,
+  SAFE_CAST(fob_import AS float64) AS fob_import,
+  SAFE_CAST(updated_xentral_sellable_stock AS int64) AS updated_xentral_sellable_stock, 
+  gojungo_sku
 FROM `leslunes-raw.products.masterlist` 
 WHERE product_name IS NOT NULL
