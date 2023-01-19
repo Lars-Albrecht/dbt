d2ef84e1b3887b9e3b2be99383d35cf2ee088033
Stefan Worthmann <admin@leslunes.de>
2023-01-19 19:58:58 +0000

    remodel creating unique_items (adjust filtering '_ll'_*'

--- models/staging/items/unique_items_de.sql
+++ models/staging/items/unique_items_de.sql
@@ -1,7 +1,7 @@
-SELECT * FROM (
-#Line items
-SELECT * EXCEPT(row_number) FROM 
+WITH line_items AS
+(SELECT * EXCEPT(row_number) FROM 
    (SELECT row_number() OVER (PARTITION BY line_items.value. id, prop.value.name, line_items.value. sku ORDER BY updated_at DESC) AS row_number,
+             id AS order__id,
           line_items.value. id AS line_items__id,
           line_items.value. variant_id AS line_items__variant_id,
           SAFE_CAST(line_items.value. quantity AS INT64) AS line_items__quantity,
@@ -50,19 +50,32 @@ SELECT * EXCEPT(row_number) FROM
           line_items.value.destination_location. address2 AS line_items__destination_location__address2,
           line_items.value.destination_location. city AS line_items__destination_location__city,
           line_items.value.destination_location. zip AS line_items__destination_location__zip,
-          id AS order__id,
           updated_at AS updated_at,
+          created_at AS created_at,
           #prop.value.value AS line_items_properties__value,
           #prop.value.name AS line_items_properties__name,
           'ITEM' AS item_type
     FROM  `leslunes-raw.shopify_de.orders`
     LEFT JOIN UNNEST(line_items) AS line_items
     LEFT JOIN UNNEST(line_items.value.properties) AS prop) AS A
-WHERE row_number = 1 AND line_items__name!='ll_fg' AND line_items__name!='ll_min_total' AND line_items__name!='_ll_coupon_info') 
-UNION ALL
-#Sets
-(SELECT * EXCEPT(row_number) FROM 
+WHERE row_number = 1 AND line_items__name!='ll_fg' AND line_items__name!='ll_min_total' 
+),
+
+line_items_filtered AS (
+   SELECT * 
+EXCEPT(row_number) 
+FROM 
+ (SELECT *, row_number() OVER (PARTITION BY order__id, line_items__title, line_items__sku ORDER BY line_items__name ASC) AS row_number,
+FROM line_items)
+WHERE row_number = 1 
+AND line_items__sku IS NOT NULL
+),
+
+sets as (SELECT * 
+EXCEPT(row_number) 
+FROM 
    (SELECT row_number() OVER (PARTITION BY line_items.value. id, line_items.value. sku ORDER BY updated_at DESC) AS row_number,
+             id AS order__id,
           line_items.value. id AS line_items__id,
           line_items.value. variant_id AS line_items__variant_id,
           SAFE_CAST(line_items.value. quantity AS INT64) AS line_items__quantity,
@@ -110,14 +123,19 @@ UNION ALL
           line_items.value.destination_location. address2 AS line_items__destination_location__address2,
           line_items.value.destination_location. city AS line_items__destination_location__city,
           line_items.value.destination_location. zip AS line_items__destination_location__zip,
-          id AS order__id,
+
           updated_at AS updated_at,
+          created_at AS created_at,
           #SAFE_CAST(NULL AS STRING) AS line_items_properties__value,
           #SAFE_CAST(NULL AS STRING) line_items_properties__name,
           'SET' AS item_type
     FROM `leslunes-raw.shopify_de.orders`
     LEFT JOIN UNNEST(line_items) AS line_items
     LEFT JOIN UNNEST(line_items.value.properties) AS prop
-    WHERE (line_items.value. sku='' OR UPPER(line_items.value. sku) LIKE '%SET' OR UPPER(line_items.value. sku) LIKE '%BUNDLE%' OR UPPER(line_items.value. sku) LIKE '%DUO%' OR UPPER(line_items.value. sku) LIKE '%SET-2021' OR UPPER(line_items.value. sku) LIKE '%TRIO') AND prop.value.value IS NOT NULL) AS B
-WHERE row_number = 1 AND line_items__name!='ll_fg' AND line_items__name!='ll_min_total' AND line_items__name!='_ll_coupon_info') 
-ORDER BY line_items__id DESC 
+    WHERE (line_items.value. sku='' OR UPPER(line_items.value. sku) LIKE '%SET' OR UPPER(line_items.value. sku) LIKE '%BUNDLE%' OR UPPER(line_items.value. sku) LIKE '%DUO%' OR UPPER(line_items.value. sku) LIKE '%SET-2021' OR UPPER(line_items.value. sku) LIKE '%TRIO') AND prop.value.value IS NOT NULL ) AS B
+WHERE row_number = 1 AND line_items__name!='ll_fg' AND line_items__name!='ll_min_total' AND line_items__name!='_ll_coupon_info')
+
+SELECT * FROM line_items_filtered
+UNION ALL 
+SELECT * FROM sets
+ORDER BY line_items__id DESC
